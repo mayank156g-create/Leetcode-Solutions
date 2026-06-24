9fb3f2a1fefe68fe50321e5a32187768052aeede
mayank156g-create <mayank156g@gmail.com>
2026-06-24 21:38:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (82.96%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+
+        if(s.size() != goal.size())
+            return false;
+
+        for(int i = 0; i < s.size(); i++) {
+
+            char first = s[0];
+            s.erase(s.begin());
+            s.push_back(first);
+
+            if(s == goal)
+                return true;
+        }
+
+        return s == goal;
+    }
+};

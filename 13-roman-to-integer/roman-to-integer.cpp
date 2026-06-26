469963acff03217c8e05175f2a0b7222246ccacf
mayank156g-create <mayank156g@gmail.com>
2026-06-26 22:13:13 +0530

    Time: 8 ms (41.74%) | Memory: 13.3 MB (37.50%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -2,57 +2,24 @@ class Solution {
 public:
     int romanToInt(string s) {
 
+        unordered_map<char,int> mp = {
+            {'I',1},
+            {'V',5},
+            {'X',10},
+            {'L',50},
+            {'C',100},
+            {'D',500},
+            {'M',1000}
+        };
+
         int ans = 0;
 
-        for(int i = 0; i < s.size(); i++) {
+        for(int i=0;i<s.size();i++){
 
-            if(i + 1 < s.size()) {
-
-                if(s[i]=='I' && s[i+1]=='V'){
-                    ans += 4;
-                    i++;
-                }
-                else if(s[i]=='I' && s[i+1]=='X'){
-                    ans += 9;
-                    i++;
-                }
-                else if(s[i]=='X' && s[i+1]=='L'){
-                    ans += 40;
-                    i++;
-                }
-                else if(s[i]=='X' && s[i+1]=='C'){
-                    ans += 90;
-                    i++;
-                }
-                else if(s[i]=='C' && s[i+1]=='D'){
-                    ans += 400;
-                    i++;
-                }
-                else if(s[i]=='C' && s[i+1]=='M'){
-                    ans += 900;
-                    i++;
-                }
-                else {
-
-                    if(s[i]=='I') ans+=1;
-                    else if(s[i]=='V') ans+=5;
-                    else if(s[i]=='X') ans+=10;
-                    else if(s[i]=='L') ans+=50;
-                    else if(s[i]=='C') ans+=100;
-                    else if(s[i]=='D') ans+=500;
-                    else ans+=1000;
-                }
-            }
-            else {
-
-                if(s[i]=='I') ans+=1;
-                else if(s[i]=='V') ans+=5;
-                else if(s[i]=='X') ans+=10;
-                else if(s[i]=='L') ans+=50;
-                else if(s[i]=='C') ans+=100;
-                else if(s[i]=='D') ans+=500;
-                else ans+=1000;
-            }
+            if(i+1<s.size() && mp[s[i]] < mp[s[i+1]])
+                ans -= mp[s[i]];
+            else
+                ans += mp[s[i]];
         }
 
         return ans;

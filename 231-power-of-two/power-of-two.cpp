63bd6cbbea53bfbba98c3b9ff7303d9ee6ad6b62
mayank156g-create <mayank156g@gmail.com>
2026-06-28 19:43:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (49.26%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -4,12 +4,6 @@ public:
         if(n <= 0)
             return false;
 
-        while(n > 1) {
-            if(n % 2 != 0)
-                return false;
-            n /= 2;
-        }
-
-        return true;
+        return (n & (n - 1)) == 0;
     }
 };

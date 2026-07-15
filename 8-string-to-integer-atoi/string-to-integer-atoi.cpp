e7f897c3655b46e581ea1aef3dc1d62d6ee01886
mayank156g-create <mayank156g@gmail.com>
2026-07-15 22:10:14 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (82.17%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -2,47 +2,37 @@ class Solution {
 public:
     int myAtoi(string s) {
 
-        int n = s.size();
         int i = 0;
+        int n = s.size();
 
-        // Skip spaces
         while (i < n && s[i] == ' ')
             i++;
 
-        // Sign
         int sign = 1;
+
         if (i < n && (s[i] == '+' || s[i] == '-')) {
             if (s[i] == '-')
                 sign = -1;
             i++;
         }
 
-        // Skip leading zeros
-        while (i < n && s[i] == '0')
-            i++;
-
-        string digits;
+        long long ans = 0;
 
         while (i < n && isdigit(s[i])) {
-            digits += s[i];
+
+            int digit = s[i] - '0';
+
+            ans = ans * 10 + digit;
+
+            if (sign == 1 && ans > INT_MAX)
+                return INT_MAX;
+
+            if (sign == -1 && -ans < INT_MIN)
+                return INT_MIN;
+
             i++;
         }
 
-        if (digits.empty())
-            return 0;
-
-        long long num = 0;
-
-        for (char c : digits) {
-            num = num * 10 + (c - '0');
-
-            if (sign == 1 && num > INT_MAX)
-                return INT_MAX;
-
-            if (sign == -1 && -num < INT_MIN)
-                return INT_MIN;
-        }
-
-        return sign * num;
+        return sign * ans;
     }
 };
